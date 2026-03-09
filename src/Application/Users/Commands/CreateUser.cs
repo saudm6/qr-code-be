ff709e8bce19f451aae660935ca919b9067ca7f3
Saud Al Maawali <soud111mawali@gmail.com>
2026-03-09 21:13:42 +0400

    Added minimal changes

--- src/Application/Users/Commands/CreateUser.cs
+++ src/Application/Users/Commands/CreateUser.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace qr_code.Domain.Entities;
+namespace qr_code.Application.Users.Commands;
 
-public class User : BaseAuditableEntity
+public class CreateUser : IRequest<Guid>
 {
     public required string FullName { get; set; }
     public required string Email { get; set; }
-    public required string PhoneNumber { get; set; }
     public required string Password { get; set; }
+    public required string PhoneNumber { get; set; }
 }
+
+public class CreateUserHandler : IRequestHandler<CreateUser, Guid>
+{
+
+}
+
---
+++ src/Application/Users/DTO/UserDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using qr_code.Domain.Entities;
+
+namespace qr_code.Application.Users.DTO;
+
+public class UserDto
+{
+    public string? FullName { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+    public string? PhoneNumber { get; set; }
+
+    public class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<User, UserDto>();
+        }
+    }
+
+
+}
